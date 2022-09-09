e715d407fd850ea455176f78fa0dfad75b094d87
Victor Akachukwu <victorakachukwu12@gmail.com>
2022-09-09 07:58:50 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase "programming is like building a multilingual puzzle"
+ * return (0) if exited properly, non-zero if other wise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+}
