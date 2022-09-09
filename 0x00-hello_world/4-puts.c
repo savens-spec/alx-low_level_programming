da2478594e519af20f73e79a4a68df385bf9af4a
Victor Akachukwu <victorakachukwu12@gmail.com>
2022-09-09 09:21:36 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 /**
 *main - print the string in the put function
-*
-*Description: using the main function 
-*this program prints "Programming is like building a multilingual puzzle
+*Description:using the main function
+*this program prints "Programming is like building a multilingual puzzle"
 *Return: 0
 */
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print the sring in the put function
+ *
+ * description: using the main function
+ * the program prints "with proper grammar, but the outcome is a piece of art"
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
