6dc96b3c0c95c9b4bd09cf3e1059b86516ac9889
Victor Akachukwu <victorakachukwu12@gmail.com>
2022-09-12 20:26:03 -0700

    task 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	els if (l == 0)
+	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
