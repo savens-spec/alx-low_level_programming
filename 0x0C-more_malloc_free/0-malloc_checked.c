0c8954e78947a87877950505bed739c5979d91e6
Victor <100317843+savens-spec@users.noreply.github.com>
2022-10-08 02:07:29 +0100

    Update 0-malloc_checked.c
    trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,20 @@
+#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 
-/*
- * malloc_checked - allocates memory using malloc
- * @b: memory to allocate
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
- * return: pointer to allocated memory or normal process termination if error
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *ptr;
 
-	p = malloc(b);
-	if (p == NULL)
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
 
-	return (p);
+	return (ptr);
 }
